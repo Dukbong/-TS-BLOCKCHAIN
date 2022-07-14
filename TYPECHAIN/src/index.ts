7465240bce5b4102f253048f12b336bf8ae8359c
HyeonSung Jang <gorae930301@gmail.com>
2022-07-15 02:43:31 +0900

    TS-PRE-STUDY

--- TYPECHAIN/src/index.ts
+++ TYPECHAIN/src/index.ts
@@ -8,6 +8,7 @@
 //     url:"jam",
 // })
 import crypto from "crypto";
+// 외부 라이브러리가 인식을 못할때 npm i -D @types/node를 설치한다.
 
 interface BlockShape{
     hash: string,
@@ -23,9 +24,38 @@ class Block implements BlockShape{
         public height: number,
         public data: string,
     ){
-        this.hash = calculateHash(prevHash, height, data);
+        this.hash = Block.calculateHash(prevHash, height, data);
     }
-    static calculateHash (prevHash: string, height: NumberConstructor, data: string){
+    static calculateHash (prevHash: string, height: number, data: string){
         const toHash = `${prevHash}${height}${data}`;
+        return crypto.createHash("sha256").update(toHash).digest("hex");
     }
 }
+
+class Blockchain{
+    public blocks:Block[];
+    constructor(){
+        this.blocks = [];
+    }
+    
+    private getPrevHash(){
+        if(this.blocks.length === 0){
+            return "";
+        }
+        return this.blocks[this.blocks.length - 1].hash;
+    }
+
+    public addBlock(data: string){
+        const newBlock = new Block(this.getPrevHash(),this.blocks.length + 1,data);
+        this.blocks.push(newBlock);
+    }
+
+    public getBlocks(){
+        return [...this.blocks];
+    }
+}
+
+let blockchain = new Blockchain();
+blockchain.addBlock("One");
+blockchain.addBlock("Two");
+console.log(blockchain);
